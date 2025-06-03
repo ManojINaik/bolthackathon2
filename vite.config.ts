c5711d79cf211d951dfc0e90e03c46ae36b177bb
Manoj Naik <68473696+ManojINaik@users.noreply.github.com>
2025-06-03 11:39:37 +0530

    Fix robot image loading in production

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,14 @@ const config: UserConfig = {
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[ext]'
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        }
       }
     }
   },
