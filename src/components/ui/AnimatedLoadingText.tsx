f384d3426c21f5e7882870637d0167127cd4d445
ManojINaik <naik97059@gmail.com>
2025-06-07 19:29:22 +0530

    loading animatiaon text centered

--- src/components/ui/AnimatedLoadingText.tsx
+++ src/components/ui/AnimatedLoadingText.tsx
@@ -13,7 +13,7 @@ export default function AnimatedLoadingText({ message, className = '' }: Animate
   }, [message]);
 
   return (
-    <div id="animatedText" className={`animated-loading-text ${className}`}>
+    <div className={`animated-loading-text flex items-center justify-center ${className}`}>
       {displayMessage.split('').map((char, index) => (
         <span key={`${char}-${index}`} style={{ animationDelay: `${0.09 * index}s` }}>
           {char === ' ' ? '\u00A0' : char}
