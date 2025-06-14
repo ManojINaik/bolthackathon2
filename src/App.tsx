479bd65bb9dc959b3af1a7e01b2c9d9d9e0450aa
Manoj Naik <68473696+ManojINaik@users.noreply.github.com>
2025-06-14 22:41:19 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function App() {
             <main className="relative z-10 max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
               <HeroSection />
             </main>
-            <LogoMarquee className="mt-[-6rem]" />
+            <LogoMarquee className="mt-[-4rem]" />
             <main className="relative z-10 max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
               <FeaturesGrid />
               <DemoSection />
